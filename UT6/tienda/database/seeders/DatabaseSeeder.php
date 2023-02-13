df3d09e8125579c6454aa8879ff918a5fbf7331f
Javier Cabezas <javiercp69@educastur.es>
2023-02-13 11:50:37 +0100

    Actualizacion

--- UT6/tienda/database/seeders/DatabaseSeeder.php
+++ UT6/tienda/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         //Creacion de los usuarios
+         $this->call(UserSeeder::class);
     }
 }
